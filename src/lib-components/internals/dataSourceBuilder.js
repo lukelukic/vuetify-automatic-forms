d46f4eb1e27474495ebed249ae591744955ce3ed
Luka <luka.lukic@quadro-tnation.com>
2020-11-03 12:36:35 +0100

    removed console.log

--- src/lib-components/internals/dataSourceBuilder.js
+++ src/lib-components/internals/dataSourceBuilder.js
@@ -1,7 +1,6 @@
 var axios = undefined
 
 async function buildDataSource(formElement) {
-  console.log(formElement)
   if (Array.isArray(formElement.dataSource)) {
     return formElement.props?.multiple
       ? formElement.dataSource
